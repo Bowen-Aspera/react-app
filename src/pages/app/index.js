ded98df7aeb5ba6f3760910bd1e2028f3aabdda7
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 23:42:30 +0800

    Update index.js

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,95 +1,79 @@
-import React, { useEffect, useState } from "react";
-// import axios from "../../plugins/axios";
+import React from "react";
+import axios from "../../plugins/axios";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
 
 function Application() {
-    const [users, setUsers] = useState([]); // state to store users data
-    const navigate = useNavigate();
-
-    useEffect(() => {
+    function UserInfo() {
         const token = localStorage.getItem("token");
-        const apiGet = () => {
-            fetch("http://127.0.0.1:8000/api/v1/accounts/users/", {
+        return axios
+            .get("http://localhost:8000/api/v1/accounts/users/me/", {
                 headers: {
-                    Authorization: `Token ${token}`,
+                    Authorization: "Token " + token,
                 },
             })
-                .then((response) => response.json())
-                .then((json) => setUsers(json)); // store users data in state
-        };
-        apiGet();
-        console.log(token)
-    }, []);
+            .then((response) => {
+                return response.data;
+            });
+    }
 
+    const { data, isLoading, error } = useQuery("user", UserInfo, { retry: 0 });
+    const navigate = useNavigate();
 
+    if (isLoading && !error) {
+        return (
+            <div style={userInfoStyle}>
+                <p>Loading...</p>
+            </div>
+        );
+    } else if (error) {
+        return (
+            <div style={userInfoStyle}>
+                <p>An error has occurred</p>
+            </div>
+        );
+    }
 
     return (
-        <>
-            <h1 style={titleStyle}>PROFILE PROFILE PROFILE</h1>
-            <h1 style={subtitleStyle}>PROFILE HERE</h1>
-
-            {users.length > 0 ? (
-                <div style={profileContainerStyle}>
-                    {users.map((user, index) => (
-                        <div key={index} style={profileItemStyle}>
-                            <p>
-                                <strong>User ID:</strong> {user.id}
-                            </p>
-                            <p>
-                                <strong>Email:</strong> {user.email}
-                            </p>
-                            <p>
-                                <strong>First Name:</strong> {user.first_name} {/* Display first name */}
-                            </p>
-                            <p>
-                                <strong>Last Name:</strong> {user.last_name} {/* Display last name */}
-                            </p>
-                        </div>
-                    ))}
-                </div>
-            ) : (
-                <div>Loading...</div>
-            )}
-
-            <button onClick={() => navigate("../")} style={logoutBtnStyle}>
+        <div style={userInfoStyle}>
+            <h1>PROFILE PROFILE PROFILE</h1>
+            <h1>PROFILE HERE</h1>
+            <p>Username: {data.username}</p>
+            <p>Email: {data.email}</p>
+            <p>User ID: {data.id}</p>
+            <p>First Name: {data.first_name}</p>
+            <p>Last Name: {data.last_name}</p>
+            <p>Date Joined: {data.date_joined}</p>
+            <button
+                onClick={() => navigate("../")}
+                style={buttonStyle}
+            >
                 Log out
             </button>
-
-
-        </>
+        </div>
     );
 }
 
-// Define block styles as objects
-const titleStyle = {
+const userInfoStyle = {
+    margin: "1rem",
+    fontFamily: "Arial, sans-serif",
+    fontSize: "1rem",
+    fontWeight: "normal",
+    color: "#333",
     textAlign: "center",
-    fontSize: "24px",
 };
 
-const subtitleStyle = {
-    textAlign: "center",
-    fontSize: "18px",
-};
-
-const logoutBtnStyle = {
-    display: "block",
-    margin: "0 auto",
-    marginTop: "16px",
-    padding: "8px 16px",
-    backgroundColor: "blue",
-    color: "white",
+const buttonStyle = {
+    background: "#007bff",
+    color: "#fff",
+    border: "none",
     borderRadius: "4px",
+    padding: "0.5rem 1rem",
+    marginTop: "1rem",
     cursor: "pointer",
+    fontSize: "1rem",
 };
 
-const profileItemStyle = {
-    color: "black",
-    margin: "16px",
-};
 
-const profileContainerStyle = {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-};
+
 export default Application;
