b783a46a6a89f1435593171422bf192bff4dc39f
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 16:35:24 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App"
+import Application from "../pages/app";
+import Register from "../components/authentication/registration";
+import MainPage from "../components/authentication/mainPage";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:<App/>
+    },
+    {
+        path: '/application',
+        element: <Application/>
+    },
+    {
+        path: "/registration",
+        element:<Register/>
+    },
+    {
+        path:'/mainPage',
+        element:<MainPage/>
+    },
 
+])
+
+export default router
