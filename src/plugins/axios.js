d316f1e1b7c1334936ef4c7612ad7ff3047dcd1c
warpakner123 <130353421+warpakner123@users.noreply.github.com>
2023-04-17 16:36:08 +0800

    Create axios.js

---
+++ src/plugins/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL='http://127.0.0.1:8000/api/v1/' // may vary in end user
+axios.defaults.headers.post['Content-Type']='application/json'
+
+
+
+export default axios
